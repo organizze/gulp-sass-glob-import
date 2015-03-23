1438ab275b7deb0380c3158a5402b02299e7e62d
Patrick Davies <me@patrickdavies.ca>
2015-03-23 16:51:26 -0400

    fixed tests

--- test/test.js
+++ test/test.js
@@ -8,8 +8,8 @@ describe('gulp-sass-glob-import', function() {
 
     it('should parse a single directory', function(done) {
 
-        var equalString = '@import "import-folder/f1.scss' + '";\n';
-        equalString += '@import "import-folder/f2.scss' + '";\n';
+        var equalString = '@import "import-folder/_f1.scss' + '";\n';
+        equalString += '@import "import-folder/_f2.scss' + '";\n';
 
         vinyl
             .src(__dirname + '/test-scss/app.scss')
@@ -26,9 +26,9 @@ describe('gulp-sass-glob-import', function() {
     it('should parse a directory recursively', function(done) {
 
 
-        var equalString = '@import "recursive-folder/f1.scss' + '";\n';
-        equalString += '@import "recursive-folder/f2.scss' + '";\n';
-        equalString += '@import "recursive-folder/nested-folder/f3.scss' + '";\n';
+        var equalString = '@import "recursive-folder/_f1.scss' + '";\n';
+        equalString += '@import "recursive-folder/_f2.scss' + '";\n';
+        equalString += '@import "recursive-folder/nested-folder/_f3.scss' + '";\n';
 
         vinyl
             .src(__dirname + '/test-scss/recursive.scss')
